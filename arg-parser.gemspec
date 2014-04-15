4e8a82a81c1b2ad8ebc76f60c51c6490793ec7a0
evilemons <stefanbay@me.com>
2014-04-15 19:52:46 -0400

    Add missing files

---
+++ arg-parser.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+	s.name = 'arg-parser'
+	s.version = '0.0.1'
+	s.date = '2014-04-13'
+	s.summary = "A library for finding options and arguments from the comman line."
+	s.descripiton = "The ArgParser class automatically finds options and arguments for options."
+	s.authors = ["Stefan Bay (evilemons)"]
+	s.email = 'stefanbay@me.com'
+	s.files = ["lib/Parser.rb"]
+	s.license = 'MIT'
+end
---
+++ lib/arg-parser.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+class ParseError < StandardError
+	attr_reader :object
+
+	def initialize object
+		@object = object
+	end
+end
+
+class ArgParser
+	def initialize arg #(array)
+		@argv= arg
+		@options = Hash.new
+		@argv.each_with_index do |x, i|
+			@options[x] = i
+		end
+	end
+	def option_exist? option
+		if @options[option]
+			true
+		else
+			false
+		end
+	end
+	def get_arg_for_option option
+		unless option_exist? option
+			raise ParseError.new("NoOption"), "There is no option: '#{option}'"
+		end
+		id = @options[option]
+		id += 1
+		arg = @argv[id]
+		unless arg
+			raise ParseError.new("NoArgument"), "No argument for option '#{option}'"
+		end
+		if arg[0] == "-"
+			raise ParseError.new("ArgIsOption"), "'#{arg}' is not an argument, tis an option"
+		end
+		arg
+	end
+	def arg_for_option option
+		opt = self.get_arg_for_option option
+		yield opt
+	end
+end
+
+if __FILE__ == $0
+
+	x = ArgParser.new ARGV
+	x.arg_for_option "-b" do |x|
+		puts x
+	end
+
+end
